c4619a7efc5ca135ca8d4bb5da0b1e938ea7631d
Sasi Kumar T <32474530+Sasikumar3096@users.noreply.github.com>
2019-08-18 12:08:45 +0530

    Create SumOfAsciiVauesandPrime.java

---
+++ SumOfAsciiVauesandPrime.java
@@ -0,0 +1,39 @@
+
+public class SumOfAsciiVauesandPrime {
+	static int odd_sum_value=0,even_sum_value=0;
+	public static void main(String[] args) {
+
+			char s[] = "abc".toCharArray();
+			for(int i=0;i<s.length;i++)
+				if(i+1%2==0)
+					odd_sum(s[i]);
+				else
+					even_sum(s[i]);
+			if(is_prime(Math.abs(odd_sum_value - even_sum_value)))
+				System.out.println("IS PRIME");
+			else
+				System.out.println("NOT PRIME");
+	}
+
+	private static boolean is_prime(int n) {
+		if(n==1)
+			return false;
+		if(n==2)
+			return true;
+		for(int i=2;i<Math.sqrt(n)+1;i++)
+			if(n%2==0)
+				return false;
+		return true;
+		
+	}
+
+	private static void odd_sum(int c) {
+		// TODO Auto-generated method stub
+		odd_sum_value+=c;
+	}
+	private static void even_sum(int c) {
+		// TODO Auto-generated method stub
+		even_sum_value+=c;
+	}
+
+}
